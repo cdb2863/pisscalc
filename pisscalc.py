e8a84815b53a704f68c139ccd2c17fd4a22f93fa
Chris Beliveau <cdb2863@rit.edu>
2022-09-29 00:05:52 -0400

    implements pisscalc.py skeleton

---
+++ pisscalc.py
@@ -0,0 +1,11 @@
+# Python Internet Subnet(S) Calculator
+# Well written subnet calculator for those who flunked out of networking school, or those whom'st have since lost the networking juice
+# © 2022 Christopher D. Beliveau
+import argparse
+import ipaddress as ip
+
+def main():
+    return
+
+if __name__ == '__main__':
+    main()
